e6de22df35738458b73a1c8b5ce6e63590c42c46
Pieterjan C <pieterjan.carly@gmail.com>
2016-02-29 11:23:13 +0100

    conditional switch number component on decimal amount

--- addon/components/output-field-number.js
+++ addon/components/output-field-number.js
@@ -1,12 +1,4 @@
 import Ember from 'ember';
-import NumberFieldComponent from '../mixins/component-field-number';
 import FieldOutputComponent from '../mixins/component-field-output-super';
 
-export default Ember.Component.extend(NumberFieldComponent, FieldOutputComponent, {
-  formatGetter: function(value) {
-    if (!Ember.isEmpty(value)) {
-      return numeral(value).format(this.get('numberFormat'));
-    }
-    return value;
-  }
-});
+export default Ember.Component.extend(FieldOutputComponent);
