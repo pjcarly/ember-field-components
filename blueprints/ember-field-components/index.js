30d403f84511fded110ae4ec6529c1472ef2001c
Pieterjan C <pieterjan.carly@gmail.com>
2016-03-01 17:39:19 +0100

    Working blueprint, bower dependencies installed automatically

--- blueprints/ember-field-components/index.js
+++ blueprints/ember-field-components/index.js
@@ -2,7 +2,15 @@ module.exports = {
   normalizeEntityName: function() {},
 
   afterInstall: function() {
-    this.addBowerPackageToProject('pen', '^0.2.2');
-    this.addBowerPackageToProject('bootstrap-select', '^1.9.4');
+    return this.addBowerPackageToProject('bootstrap-select', '^1.9.4')
+      .then(function() {
+        return this.addBowerPackageToProject('moment', '^2.11.2');
+      }.bind(this))
+      .then(function(){
+        return this.addBowerPackageToProject('eonasdan-bootstrap-datetimepicker', '^4.17.37');
+      }.bind(this))
+      .then(function(){
+        return this.addBowerPackageToProject('ua-parser-js', '^0.7.10');
+      }.bind(this));
   }
 };
--- index.js
+++ index.js
@@ -6,14 +6,14 @@ module.exports = {
   included: function(app) {
     this._super.included(app);
 
-    if(app.import){
-      app.import(app.bowerDirectory + '/moment/moment.js');
-      
-      app.import(app.bowerDirectory + '/bootstrap-select/dist/js/bootstrap-select.js');
-      app.import(app.bowerDirectory + '/bootstrap-select/dist/css/bootstrap-select.css');
+    app.import(app.bowerDirectory + '/moment/moment.js');
+    app.import(app.bowerDirectory + '/ua-parser-js/src/ua-parser.js');
 
-      app.import(app.bowerDirectory + '/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js');
-      app.import(app.bowerDirectory + '/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.css');
-    }
+    app.import(app.bowerDirectory + '/bootstrap-select/dist/js/bootstrap-select.js');
+    app.import(app.bowerDirectory + '/bootstrap-select/dist/css/bootstrap-select.css');
+
+    app.import(app.bowerDirectory + '/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js');
+    app.import(app.bowerDirectory + '/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.css');
+
   }
 };
