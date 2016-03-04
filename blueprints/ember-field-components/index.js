caf36b75f8bb88797d824eac8fa15de9441631aa
dylanmensaert <dylan.mensaert@cloudinnovation.be>
2016-03-04 17:38:26 +0100

    Further changes

--- blueprints/ember-field-components/index.js
+++ blueprints/ember-field-components/index.js
@@ -1,4 +1,8 @@
 module.exports = {
+    // Fix: https://github.com/ember-cli/ember-cli/pull/3846/files
+    normalizeEntityName: function (entityName) {
+        return entityName;
+    },
     afterInstall: function () {
         var addBowerPackageToProject = this.addBowerPackageToProject.bind(this);
 
