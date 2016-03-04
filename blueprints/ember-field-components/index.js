48a0cca7cadcd60ef6193555f8bf08fc02bead58
dylanmensaert <dylan.mensaert@cloudinnovation.be>
2016-03-04 11:55:03 +0100

    Refactor.

--- blueprints/ember-field-components/index.js
+++ blueprints/ember-field-components/index.js
@@ -1,16 +1,14 @@
 module.exports = {
-  normalizeEntityName: function() {},
-
-  afterInstall: function() {
-    return this.addBowerPackageToProject('bootstrap-select', '^1.9.4')
-      .then(function() {
-        return this.addBowerPackageToProject('moment', '^2.11.2');
-      }.bind(this))
-      .then(function(){
-        return this.addBowerPackageToProject('eonasdan-bootstrap-datetimepicker', '^4.17.37');
-      }.bind(this))
-      .then(function(){
-        return this.addBowerPackageToProject('ua-parser-js', '^0.7.10');
-      }.bind(this));
-  }
+    afterInstall: function () {
+        return this.addBowerPackageToProject('bootstrap-select', '^1.9.4')
+            .then(function () {
+                return this.addBowerPackageToProject('moment', '^2.11.2');
+            }.bind(this))
+            .then(function () {
+                return this.addBowerPackageToProject('eonasdan-bootstrap-datetimepicker', '^4.17.37');
+            }.bind(this))
+            .then(function () {
+                return this.addBowerPackageToProject('ua-parser-js', '^0.7.10');
+            }.bind(this));
+    }
 };
