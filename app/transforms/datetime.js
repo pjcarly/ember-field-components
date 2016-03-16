9e048f516287b4f01eea002508e9dec774f2d983
dylanmensaert <dylan.mensaert@cloudinnovation.be>
2016-03-16 17:48:30 +0100

    Small changes.

--- app/transforms/datetime.js
+++ app/transforms/datetime.js
@@ -1,10 +1,23 @@
+/* global moment: true */
+import Ember from 'ember';
 import DS from 'ember-data';
 
 export default DS.Transform.extend({
-  deserialize: function(serialized) {
-    return serialized;
-  },
-  serialize: function(deserialized) {
-    return deserialized;
-  }
+    deserialize: function (serialized) {
+        if (Ember.isEmpty(serialized)) {
+            serialized = null;
+        }
+
+        return moment(serialized).unix();
+    },
+    serialize: function (deserialized) {
+        var serialized = null,
+            dateTime = moment.unix(deserialized);
+
+        if (dateTime.isValid()) {
+            dateTime = dateTime.toISOString();
+        }
+
+        return dateTime;
+    }
 });
