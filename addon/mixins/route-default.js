f19878b3d3005e460ddad1dc9d9cdf85b877d703
Pieterjan C <pieterjan.carly@gmail.com>
2016-03-21 00:03:49 +0100

    Removed unused legacy code

--- addon/mixins/route-default.js
+++
@@ -1,7 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Mixin.create({
-  setPageTitle: function(title) {
-    this.controllerFor('application').setPageTitle(title);
-  }
-});
--- addon/mixins/route-entity-index.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-import EntityRoute from '../mixins/route-entity';
-
-export default Ember.Mixin.create(EntityRoute, {
-  model: function() {
-    var entityName = this.get('entityName');
-    return this.store.findAll(entityName);
-  }
-});
