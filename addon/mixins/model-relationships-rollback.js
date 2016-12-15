6ab7ed127c078a8d974d1b169ac05c208b42259c
Pieterjan C <pieterjan.carly@gmail.com>
2016-12-15 11:42:36 +0100

    Rollback relationships

--- addon/mixins/model-relationships-rollback.js
+++ addon/mixins/model-relationships-rollback.js
@@ -1,45 +1,64 @@
 import Ember from 'ember';
 
 export default Ember.Mixin.create({
-  rollbackAttributes: function() {
-    this._super(...arguments);
-    this.rollbackRelationships();
+  setOldRelationships: function() {
+    let oldRelationships = {};
+
+    Ember.run.schedule('actions', this, function() {
+      this.eachRelationship(function(name, descriptor) {
+        if (descriptor.kind === 'belongsTo') {
+          const recordId = this.belongsTo(name).id();
+          oldRelationships[name] = recordId;
+        }
+      }, this);
+
+      this.set('_oldRelationships', oldRelationships);
+    });
+  },
+  didCreate: function() {
+    this.setOldRelationships();
+  },
+  didLoad: function() {
+    this.setOldRelationships();
+  },
+  didUpdate: function() {
+    this.setOldRelationships();
   },
 
   rollbackRelationships: function() {
-    var model = this;
-    model.eachRelationship(function(name, meta) {
-      if (meta.kind === 'belongsTo') {
-        var oldId = model.get(`${name}Id`);
-        if (oldId) {
-          model.get(name).then(function(child) {
-            if (!(child && child.get('id') === oldId)) {
-              model.store.findRecord(name, oldId).then(function(originalRecord) {
-                model.set(name, originalRecord);
-              });
-            }
-          });
+    const oldRelationships = this.get('_oldRelationships');
+    this.eachRelationship(function(name, descriptor) {
+      if (descriptor.kind === 'belongsTo') {
+        const recordId = oldRelationships[name];
+        if(!Ember.isBlank(recordId)){
+          if(this.get('store').hasRecordForId(descriptor.type, recordId)) {
+            const oldRelationshipModel = this.get('store').peekRecord(descriptor.type, recordId);
+            this.set(name, oldRelationshipModel);
+          } else {
+            Ember.assert(`Tried rolling back relationshp ${name} on ${this.get('name')}, and record with ID ${recordId} of type ${descriptor.type} was not found in the store`);
+          }
+        } else {
+          this.set(name, null);
         }
       }
-    });
+    }, this);
+
+    this.setOldRelationships();
   },
 
-  // keep a cache of all belongsTo ids before there are any user-initiated changes
-  // so we can roll back manually in `rollbackAttributes`
-  cacheRelationships: function() {
-    var model = this;
-    model.eachRelationship(function(name, meta) {
-      if (meta.kind === 'belongsTo') {
-        model.get(name).then(function(child) {
-          if (child && child.get('id')) {
-            model.set(`${name}Id`, child.get('id'));
-          }
-        });
+  hasDirtyRelationships(){
+    const oldRelationships = this.get('_oldRelationships');
+    let isDirty = false;
+    this.eachRelationship(function(name, descriptor) {
+      if (descriptor.kind === 'belongsTo') {
+        const recordId = this.belongsTo(name).id();
+
+        if(oldRelationships[name] !== recordId){
+          isDirty = true;
+          return;
+        }
       }
-    });
-  },
-
-  ready: function() {
-    this.cacheRelationships();
+    }, this);
+    return isDirty;
   }
 });
