4e3e9f3409b6b9b3142fdef4b339fa98e7506cea
Pieterjan C <pieterjan.carly@gmail.com>
2017-05-22 23:23:42 +0200

    fix percent/percentage

--- addon/components/input-field-percent/component.js
+++ addon/components/input-field-percent/component.js
@@ -1,5 +1,3 @@
 import InputFieldNumber from '../../components/input-field-number/component';
 
-export default InputFieldNumber.extend({
-  suffix: '%'
-});
+export default InputFieldNumber.extend();
---
+++ app/components/input-field-percent/component.js
@@ -0,0 +1,3 @@
+import InputField from 'ember-field-components/components/input-field-percent/component';
+
+export default InputField;
