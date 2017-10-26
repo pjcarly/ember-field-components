6e5501717fa04272c0b2d45c4c1fb586beaae7ac
Pieterjan Carly <pieterjan.carly@gmail.com>
2017-10-26 23:22:13 +0200

    Changed default date format

--- addon/services/field-settings.js
+++ addon/services/field-settings.js
@@ -3,8 +3,8 @@ import Ember from 'ember';
 const { Service } = Ember;
 
 export default Service.extend({
-    dateFormat: 'DD/MM/YYYY',
-    dateTimeFormat: 'DD/MM/YYYY HH:mm:ss',
+    dateFormat: 'YYYY-MM-DD',
+    dateTimeFormat: 'YYYY-MM-DD HH:mm:ss',
     timeFormat: 'HH:mm:ss',
     locale: 'nl-BE',
     currencyDisplay: 'symbol',
