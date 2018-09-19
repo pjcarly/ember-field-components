c59ff1d3aca4c382b59fb9bc1b6099ba1cfc712c
Pieterjan C <pieterjan.carly@gmail.com>
2018-09-19 13:32:10 +0200

    Dependencies update

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ module.exports = {
 
         app.import(app.bowerDirectory + '/moment/moment.js');
 
-        app.import(app.bowerDirectory + '/bootstrap-select/dist/js/bootstrap-select.js');
-        app.import(app.bowerDirectory + '/bootstrap-select/dist/css/bootstrap-select.css');
+        // app.import(app.bowerDirectory + '/bootstrap-select/dist/js/bootstrap-select.js');
+        // app.import(app.bowerDirectory + '/bootstrap-select/dist/css/bootstrap-select.css');
 
         app.import(app.bowerDirectory + '/eonasdan-bootstrap-datetimepicker/src/js/bootstrap-datetimepicker.js');
         app.import(app.bowerDirectory + '/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.css');
