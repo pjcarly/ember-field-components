4742c3aedd1ee259daa83da17a761a5fefffc3ae
Pieterjan C <pieterjan.carly@gmail.com>
2019-03-18 18:36:56 +0100

    Dynamic computed property. Begone observers!

--- addon/components/input-field/component.ts
+++ addon/components/input-field/component.ts
@@ -1,4 +1,5 @@
 import BaseField from "../../field/BaseField";
+import { defineProperty, computed as classicComputed } from '@ember/object';
 import { computed, action } from '@ember-decorators/object';
 import { guidFor } from '@ember/object/internals';
 import { tagName } from '@ember-decorators/component';
@@ -9,20 +10,33 @@ export default class InputField extends BaseField {
    * Returns a unique dom id that can be used to identify the input element.
    * This is also bound to the label and put on the label "for" attribute
    */
-
   inputId !: string;
 
+  init() {
+    super.init();
+
+    // This is where the magic happens, we define a computed property on the instance of this component,
+    // but because the name of the field is passed in, the dependent keys of the computed property are dynamic
+    // by using defineProperty, we can set the computed property on Init of this component, with the correct dependent keys.
+
+    defineProperty(this, 'value', classicComputed('model', `model.${this.field}`, {
+      get(){
+        return this.model.get(this.field);
+      },
+      set(key, value){
+        this.model.set(this.field, value);
+        this.notifyExternalAction(value);
+        return value;
+      }
+    }));
+  }
+
+  /**
+   * Returns a unique inputId for the instance of this field
+   */
   @computed
   get calculatedInputId() : string {
     return `${guidFor(this)}-input`;
-  }
-
-  @computed('model', 'field')
-  get value() : any {
-    return this.model.get(this.field);
-  }
-  set value(value: any) {
-    this.model.set(this.field, value);
   }
 
   @computed('type')
