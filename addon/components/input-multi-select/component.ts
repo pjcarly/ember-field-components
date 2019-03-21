7e0b51a27642dc9679ff68afe7c75af80197edd4
Pieterjan C <pieterjan.carly@gmail.com>
2019-03-21 17:49:16 +0100

    working multiselect

--- addon/components/input-multi-select/component.ts
+++ addon/components/input-multi-select/component.ts
@@ -1,5 +1,35 @@
 import InputSelectComponent from '../input-select/component';
+import { computed, action } from '@ember-decorators/object';
+import { isBlank } from '@ember/utils';
+import { guidFor } from '@ember/object/internals';
 
 export default class InputMultiSelectComponent extends InputSelectComponent {
 
+  @computed('inputId')
+  get inputIdComputed() : string {
+    if(!isBlank(this.inputId)) {
+      return this.inputId;
+    } else {
+      return `${guidFor(this)}-select`;
+    }
+  }
+
+  @action
+  setValue(){
+    const element : HTMLSelectElement | null = <HTMLSelectElement> document.getElementById(this.inputIdComputed);
+    const values : Array<string> = [];
+
+    if(!element){
+      this.computedValue = values;
+      return;
+    }
+
+    for(const option of element.options) {
+      if(option.selected) {
+        values.push(option.value);
+      }
+    }
+
+    this.computedValue = values;
+  }
 }
