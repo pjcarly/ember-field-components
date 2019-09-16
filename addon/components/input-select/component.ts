b98c44c932a62f552238d8f19515619b187a8a8a
Pieterjan C <pieterjan.carly@gmail.com>
2019-09-16 10:06:28 +0200

    double equals on select lists

--- addon/components/input-select/component.ts
+++ addon/components/input-select/component.ts
@@ -1,43 +1,45 @@
-import BaseInput from '../BaseInput';
-import SelectOption from 'ember-field-components/interfaces/SelectOption';
-import { computed } from '@ember-decorators/object';
-import { isBlank } from '@ember/utils';
-import { inject as service } from '@ember-decorators/service';
+import BaseInput from "../BaseInput";
+import SelectOption from "ember-field-components/interfaces/SelectOption";
+import { computed } from "@ember-decorators/object";
+import { isBlank } from "@ember/utils";
+import { inject as service } from "@ember-decorators/service";
 
 export default class InputSelectComponent extends BaseInput {
-  @service intl !: any;
+  @service intl!: any;
 
-  type = 'select';
-  required : boolean = false;
-  noneLabel : string = '';
+  type = "select";
+  required: boolean = false;
+  noneLabel: string = "";
   selectOptions: SelectOption[] = [];
 
-  @computed('computedValue', 'required')
-  get showNone() : boolean {
+  @computed("computedValue", "required")
+  get showNone(): boolean {
     return isBlank(this.computedValue) || this.required === false;
   }
 
-  @computed('computedValue', 'required')
-  get noneDisabled() : boolean {
+  @computed("computedValue", "required")
+  get noneDisabled(): boolean {
     return isBlank(this.computedValue) && this.required;
   }
 
-  @computed('computedValue', 'required')
-  get noneSelected() : boolean {
+  @computed("computedValue", "required")
+  get noneSelected(): boolean {
     return isBlank(this.computedValue) && this.required;
   }
 
-  @computed('noneLabel', 'intl.local')
-  get noneLabelComputed() : string {
-    return isBlank(this.noneLabel) ? this.intl.t('ember-field-components.label.select_none') : this.noneLabel;
+  @computed("noneLabel", "intl.local")
+  get noneLabelComputed(): string {
+    return isBlank(this.noneLabel)
+      ? this.intl.t("ember-field-components.label.select_none")
+      : this.noneLabel;
   }
 
-  @computed('selectOptions.[]', 'value')
-  get selectOptionsContainsValue() : boolean {
+  @computed("selectOptions.[]", "value")
+  get selectOptionsContainsValue(): boolean {
     let returnValue = false;
 
-    for(const selectOption of this.selectOptions) {
-      if(selectOption.value === this.value) {
+    for (const selectOption of this.selectOptions) {
+      if (selectOption.value == this.value) {
         returnValue = true;
         break;
       }
---
+++ app/helpers/deq.ts
@@ -0,0 +1,1 @@
+export { default, equal } from "ember-field-components/helpers/double-equals";
