6fc2e71664a94832f0c41dfb281dd2512efa0ab1
Pieterjan C <pieterjan.carly@gmail.com>
2020-04-06 15:44:02 +0200

    Small refactor

--- addon/components/field-messages/component.ts
+++ addon/components/field-messages/component.ts
@@ -1,19 +1,19 @@
-import Component from '@ember/component';
-import Model from 'ember-data/model';
-import { computed } from '@ember/object';
-import { tagName } from '@ember-decorators/component';
+import Component from "@ember/component";
+import Model from "ember-data/model";
+import { computed } from "@ember/object";
+import { tagName } from "@ember-decorators/component";
 
 /**
  * This component displays the error messages for the provided field on the model
  */
-@tagName('')
+@tagName("")
 export default class FieldMessagesComponent extends Component {
-  model !: Model;
-  field !: string;
+  model!: Model;
+  field!: string;
 
-  @computed('model.errors.[]', 'field')
+  @computed("model.errors.[]", "field")
   get errors() {
-    var errors = this.model.get('errors');
+    const errors = this.model.get("errors");
     return errors.errorsFor(this.field);
   }
 }
