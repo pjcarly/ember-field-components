487f9cbe1707645ec7216ae6c01b69759c477a69
Pieterjan <pieterjan.carly@gmail.com>
2020-04-15 09:41:20 +0200

    upgraded Ember, temporary removed model-fragments

--- addon/classes/attribute.ts
+++ addon/classes/attribute.ts
@@ -1,6 +1,5 @@
 import attr from "ember-data/attr";
-// @ts-ignore
-import { fragment } from "ember-data-model-fragments/attributes";
+// import { fragment } from "ember-data-model-fragments/attributes";
 import { isNumeric } from "ember-attribute-validations/utils";
 import { assert } from "@ember/debug";
 import { assign } from "@ember/polyfills";
@@ -73,13 +72,13 @@ export function setType(type: string, options: any) {
         url: true,
       };
       break;
-    case "address":
-      options = isBlank(options) ? {} : options;
-      options.validation = options.hasOwnProperty("validation")
-        ? options.validation
-        : {};
-      options.validation.validAddress = true;
-      return fragment("address", options); // TODO fix dependency with ember-mist-components
+    // case "address":
+    //   options = isBlank(options) ? {} : options;
+    //   options.validation = options.hasOwnProperty("validation")
+    //     ? options.validation
+    //     : {};
+    //   options.validation.validAddress = true;
+    //   return fragment("address", options); // TODO fix dependency with ember-mist-components
   }
 
   // Now we have our default validations and or options
