a1e9e27dc92c1afba646cafe851708f2745714d1
Pieterjan C <pieterjan.carly@gmail.com>
2020-04-17 00:53:05 +0200

    Upgrade intl, cleanup types

--- addon/helpers/is-array.ts
+++
@@ -1,8 +0,0 @@
-import Helper from '@ember/component/helper';
-import { isArray } from '@ember/array';
-
-export default class IsArrayHelper extends Helper {
-  compute([value]: [any]) {
-    return isArray(value);
-  }
-}
