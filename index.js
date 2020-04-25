fe2a1d7c7a22dfd33a6e0fea48d3d7d0b8b60988
Pieterjan <pieterjan.carly@gmail.com>
2020-04-25 02:18:16 +0200

    cleanup dependencies

--- index.js
+++ index.js
@@ -1,19 +1,8 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  name: require('./package').name,
+  name: require("./package").name,
   isDevelopingAddon() {
     return true;
-  }
-  // included(app) {
-  //   this._super.included.apply(this, arguments);
-
-  //   app.import('node_modules/autosize/dist/autosize.js');
-  //   app.import('node_modules/bootstrap/dist/js/bootstrap.js');
-  //   app.import('node_modules/bootstrap/dist/css/bootstrap.css');
-  //   app.import('node_modules/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js');
-  //   app.import('node_modules/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css');
-  //   app.import('node_modules/bootstrap-slider/dist/bootstrap-slider.js');
-  //   app.import('node_modules/bootstrap-slider/dist/css/bootstrap-slider.css');
-  // }
+  },
 };
