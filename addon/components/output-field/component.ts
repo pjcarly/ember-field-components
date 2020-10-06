e8a16e5058315ebb47b8dd625bca2891fe4f3296
Pieterjan C <pieterjan.carly@gmail.com>
2020-10-06 23:39:01 +0200

    output-label

--- addon/components/output-field/component.ts
+++ addon/components/output-field/component.ts
@@ -1,13 +1,14 @@
-import BaseField from '../BaseField';
-import { defineProperty, computed as classicComputed } from '@ember/object';
-import { computed } from '@ember/object';
-import { dasherize } from '@ember/string';
+import BaseField from "../BaseField";
+import { defineProperty, computed as classicComputed } from "@ember/object";
+import { computed } from "@ember/object";
+import { dasherize } from "@ember/string";
 
 export default class OutputFieldComponent extends BaseField {
   /**
    * The CSS class you want to give to the output element in the DOM
    */
-  outputClass : string = '';
+  outputClass: string = "";
+  labelClass = "output-label";
 
   init() {
     super.init();
@@ -16,43 +17,47 @@ export default class OutputFieldComponent extends BaseField {
     // but because the name of the field is passed in, the dependent keys of the computed property are dynamic
     // by using defineProperty, we can set the computed property on Init of this component, with the correct dependent keys.
 
-    defineProperty(this, 'value', classicComputed('model', `model.${this.field}`, {
-      get() {
-        return this.model.get(this.field);
-      }
-    }));
+    defineProperty(
+      this,
+      "value",
+      classicComputed("model", `model.${this.field}`, {
+        get() {
+          return this.model.get(this.field);
+        },
+      })
+    );
   }
 
-  @computed('class', 'componentName')
-  get computedClass() : string {
-    const classes : string[] = [];
+  @computed("class", "componentName")
+  get computedClass(): string {
+    const classes: string[] = [];
 
-    classes.push('output-field');
+    classes.push("output-field");
     classes.push(this.componentName);
-    if(this.modelName) {
+    if (this.modelName) {
       classes.push(`${dasherize(this.modelName)}-${dasherize(this.field)}`);
     }
 
-    if(this.inline) {
-      classes.push('inline');
+    if (this.inline) {
+      classes.push("inline");
     }
 
-    if(this.class) {
+    if (this.class) {
       classes.push(this.class);
     }
 
-    return classes.join(' ');
+    return classes.join(" ");
   }
 
   /**
    * The name of the subcomponent that will be injected as the output-field. This is dependent on the type of field
    */
-  @computed('type')
-  get componentName() : string {
+  @computed("type")
+  get componentName(): string {
     let type = this.type;
 
-    if(type === 'id' || type === 'string') {
-      type = 'text';
+    if (type === "id" || type === "string") {
+      type = "text";
     }
 
     return `output-field-${type}`.toLowerCase();
@@ -61,8 +66,8 @@ export default class OutputFieldComponent extends BaseField {
   /**
    * These are options that will get bassed down to the output component
    */
-  @computed('options.outputOptions')
-  get outputOptions() : any {
+  @computed("options.outputOptions")
+  get outputOptions(): any {
     return this.options ? this.options.outputOptions : undefined;
   }
 }
