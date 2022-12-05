b6aac80b11d5bc6df6f746e34c03113d8217c4ce
Pieterjan C <pieterjan.carly@gmail.com>
2022-12-05 13:57:05 +0100

    Fix tracking issue on DS.Errors

--- addon/components/field-messages/component.ts
+++ addon/components/field-messages/component.ts
@@ -1,6 +1,7 @@
 import Component from "@glimmer/component";
 import Model from "@ember-data/model";
 import DS from "ember-data";
+import { computed, get } from "@ember/object";
 
 interface Arguments {
   model: Model;
@@ -8,8 +9,10 @@ interface Arguments {
 }
 
 export default class FieldMessagesComponent extends Component<Arguments> {
+
+  @computed("args.model.errors.[]", "args.field")
   get errors() {
-    const errors = <DS.Errors>(<unknown>this.args.model.errors);
+    const errors = get(this.args.model, 'errors') as DS.Errors;
     return errors.errorsFor(this.args.field);
   }
 }
